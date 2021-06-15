4b744f0a199bd83a823f657e046ee694cdeaa067
李玉祥 <1597307923@qq.com>
2021-06-15 16:18:52 +0800

    注册小程序和页面

--- app.js
+++ app.js
@@ -1,31 +1,55 @@
 App({
 
-
+  data: {
+    userInfo:{}
+  },
   /**
    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
    */
   onLaunch: function () {
-    
+    wx.getUserProfile({
+      desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        console.log(res);
+        this.setData({
+          userInfo: res.userInfo,
+          // hasUserInfo: true
+        })
+      }
+    })
   },
+
 
   /**
    * 当小程序启动，或从后台进入前台显示，会触发 onShow
    */
   onShow: function (options) {
-    
+    // 判断小程序进入场景
+    console.log(options);
+    switch (options.scene) {
+      case 1001:
+
+        break;
+      case 1002:
+
+        break;
+
+      default:
+        break;
+    }
   },
 
   /**
    * 当小程序从前台进入后台，会触发 onHide
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
    */
   onError: function (msg) {
-    
+
   }
 })
--- pages/home/home.js
+++ pages/home/home.js
@@ -5,16 +5,51 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    userInfo: {},
+    hasUserInfo: false,
+    canIUseGetUserProfile: false,
+    list:[]
   },
-  click(){
-    console.log("按钮点击");
+  getUserProfile(e) {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认
+    // 开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        }),
+        console.log(res);
+        console.log(this.data.userInfo);
+      }
+    })
+  },
+  getUserInfo(e) {
+    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    wx.request({
+      url: 'http://123.207.32.32:8000/home/multidata',
+      success:(res)=>{
+        console.log(res);
+        this.setData({
+          list:res.data
+        })
+      }
+    })
+    if (wx.getUserProfile) {
+      this.setData({
+        canIUseGetUserProfile: true
+      })
+    }
   },
 
   /**
@@ -64,5 +99,10 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  // 监听页面滚动到地步
+  onReachBottom:function () {
+    console.log("到底部了")
   }
 })
